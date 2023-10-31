70fd58fcb3e6b81a08f9daf8d94451809e5433f9
Bharghav Srinivasan <81548305+BharZInstein@users.noreply.github.com>
2023-10-31 23:32:56 +0530

    Debugged the errors in the code and added a few more lines to make it better

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 sum = 0
-while (True):
-    userInput = input("Enter the item price or press q to quit: \n")
-    if (userInput != 'q'):
-        sum = sum + int(userInput)
-        print(f"Order total so far: {sum}")
-    else:
-        print(f"Your Bill total is {sum}. Thanks for shopping with us"
+
+while True:
+    userInput = input("Enter the item price or press 'q' to quit: \n")
+    if userInput.lower() == 'q':
         break
+    try:
+        item_price = float(userInput)
+        if item_price < 0:
+            print("Please enter a non-negative item price.")
+        else:
+            sum += item_price
+            print(f"Order total so far: {sum}")
+    except ValueError:
+        print("Invalid input. Please enter a valid item price (a number) or 'q' to quit.")
+
+print(f"Your Bill total is ${sum:.2f}. Thanks for shopping with us.")
